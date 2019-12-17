ea3cb3ce3bd574870c8c2e7c8022a80a23559a55
Matt H <housems@hotmail.com>
2019-12-16 21:47:47 -0600

    Create V6__stage_2_table.sql

---
+++ sql/V6__stage_2_table.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+--  DDL for Table STAGE_2
+--------------------------------------------------------
+
+  CREATE TABLE "STAGE_2" 
+   ("CLAIM_ID" NUMBER,
+   "CLAIM_TYPE" VARCHAR2(20 BYTE),  
+	"CLAIM_DATE" DATE, 
+	"CLAIM_AMOUNT" NUMBER, 
+	"MEMBER_ID" VARCHAR2(20 BYTE)
+   ) ;
+
