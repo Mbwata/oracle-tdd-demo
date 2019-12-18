3535edf22f86b2ff36e9a8a607812ccbb394100e
Matt H <housems@hotmail.com>
2019-12-18 14:27:56 -0600

    Update procedure to pass test

--- sql/R__accum_engine_pkgb.sql
+++ sql/R__accum_engine_pkgb.sql
@@ -1,9 +1,23 @@
 CREATE OR REPLACE PACKAGE BODY accum_engine AS
+
     PROCEDURE move_rx_stage_1_to_stage_2 (
         iclaim_id NUMBER
     ) IS
     BEGIN
-        NULL;
+        INSERT INTO stage_2
+            ( SELECT
+                stage_2_id_seq.NEXTVAL,
+                claim_id,
+                'RX',
+                claim_date,
+                claim_amount,
+                member_id
+            FROM
+                stage_1_rx_claims
+            WHERE
+                claim_id = iclaim_id
+            );
+
     END move_rx_stage_1_to_stage_2;
 
 END accum_engine;
